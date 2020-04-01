daab96fdc117b23e493dce7f084df18e2c722537
seima-osako <kirama129@outlook.jp>
2020-04-01 11:23:31 +0900

    add

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,7 +2,7 @@ class BoardsController < ApplicationController
   before_action :set_target_board, only: %i[show edit update destroy]
 
   def index
-    @boards = Board.all
+    @boards = Board.page(params[:page])
   end
   
   def new
@@ -11,6 +11,7 @@ class BoardsController < ApplicationController
 
   def create
     board = Board.create(board_params)
+    flash[:notice] = "「#{board.title}」の掲示板を作成しました"
     # idが付与されているため、そのURLにリダイレクトするように設定
     redirect_to board
   end
@@ -30,7 +31,7 @@ class BoardsController < ApplicationController
   def destroy
     @board.delete
 
-    redirect_to boards_path
+    redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました" }
   end
 
   private
